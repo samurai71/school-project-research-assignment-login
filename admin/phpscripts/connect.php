2834bc915126233f22c959d13b57891e216a9fa6
Mark Landeryou <marklanderyou@hotmail.com>
2018-02-15 22:46:10 -0500

    updates

--- admin/phpscripts/connect.php
+++ admin/phpscripts/connect.php
@@ -3,12 +3,12 @@
 	$user = "root";
 	$pass = "root";
 	$url = "localhost";
-	$db = "db_cms-login";
-	
+	$db = "db_login";
+
 	$link = mysqli_connect($url, $user, $pass, $db, "8889"); //Mac
 	//$link = mysqli_connect($url, $user, $pass, $db); //PC
-	
-	/* check connection */ 	
+
+	/* check connection */
 	if(mysqli_connect_errno()) {
 		printf("Connect failed: %s\n", mysqli_connect_error());
 		exit();
