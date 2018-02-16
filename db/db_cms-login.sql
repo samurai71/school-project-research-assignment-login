cb760f2bbaef11e5c8e92eafdd0d0bb34e872677
Mark Landeryou <marklanderyou@hotmail.com>
2018-02-15 22:56:30 -0500

    updated database

--- db/db_cms-login.sql
+++ db/db_cms-login.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Jan 26, 2017 at 04:46 PM
--- Server version: 5.5.42
--- PHP Version: 5.6.10
+-- Host: localhost
+-- Generation Time: Feb 16, 2018 at 04:52 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `LoginAttempts` (
   `IP` varchar(20) NOT NULL,
   `Attempts` int(11) NOT NULL,
-  `LastLogin` datetime NOT NULL
+  `LastLogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -52,7 +52,7 @@ CREATE TABLE `tbl_sessions` (
 --
 
 CREATE TABLE `tbl_user` (
-  `user_id` smallint(4) unsigned NOT NULL,
+  `user_id` smallint(4) UNSIGNED NOT NULL,
   `user_fname` varchar(50) NOT NULL,
   `user_lname` varchar(50) NOT NULL,
   `user_name` varchar(50) NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE `tbl_user` (
   `user_level` varchar(25) NOT NULL,
   `user_ip` varchar(100) NOT NULL,
   `user_date` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
@@ -99,7 +99,7 @@ ALTER TABLE `tbl_sessions`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `user_id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `user_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
